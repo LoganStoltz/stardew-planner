68be012782c5063bd99ecb5b096deb4a92b78b18
Logan Stoltz <102991130+LoganStoltz@users.noreply.github.com>
2026-03-24 08:56:35 -0700

    Adding crop data

--- server/app.rb
+++ server/app.rb
@@ -2,6 +2,31 @@ require 'sinatra'
 require 'json'
 require 'rack/cors'
 
+def crops_data
+  file_path = File.expand_path('data/crops.json', __dir__)
+  JSON.parse(File.read(file_path))
+end
+
+def crop_harvests(crop, days)
+  growth_days = crop['growth_days'].to_i
+  return 0 if growth_days <= 0 || days < growth_days
+
+  regrow_days = crop['regrow_days']
+  return 1 if regrow_days.nil?
+
+  extra_harvests = (days - growth_days) / regrow_days.to_i
+  1 + extra_harvests
+end
+
+def crop_profit(crop, days)
+  harvests = crop_harvests(crop, days)
+  return 0 if harvests.zero?
+
+  revenue = harvests * crop['sell_price'].to_i
+  seed_cost = crop['buy_price'].to_i
+  revenue - seed_cost
+end
+
 use Rack::Cors do
   allow do
     origins '*'
@@ -14,13 +39,30 @@ get '/' do
 end
 
 get '/api/best-crops' do
-  season = params['season']
+  season = params['season']&.downcase
   days = params['days'].to_i
 
+  seasonal_crops = crops_data.select { |crop| crop['season'] == season }
+
+  scored = seasonal_crops.map do |crop|
+    harvests = crop_harvests(crop, days)
+    {
+      name: crop['name'],
+      harvests: harvests,
+      profit: crop_profit(crop, days)
+    }
+  end
+
+  best = scored.max_by { |crop| crop[:profit] }
+
   content_type :json
-  {
-    best_crop: "Strawberry",
-    profit: 960,
-    harvests: 3
-  }.to_json
+  if best.nil? || best[:profit] <= 0
+    { error: 'No profitable crop found for those inputs.' }.to_json
+  else
+    {
+      best_crop: best[:name],
+      profit: best[:profit],
+      harvests: best[:harvests]
+    }.to_json
+  end
 end
